2216f995d9cea39509756b975877a562147bc35b
Verhusha Oleg <olegverhusha@gmail.com>
2023-07-02 18:56:14 +0300

    add orange theme; fix refresh plugin, loki

--- config/build/buildRules.ts
+++ config/build/buildRules.ts
@@ -1,12 +1,17 @@
 import webpack from 'webpack';
 import { BuildOptions } from './types/config';
 import { buildCssLoader } from './loaders/buildCssLoader';
+import { buildBabelLoader } from './loaders/buildBabelLoader';
 
 export function buildRules(options: BuildOptions): webpack.RuleSetRule[] {
   const svgLoader = {
     test: /\.svg$/,
     use: ['@svgr/webpack'],
   };
+
+  const cssLoader = buildCssLoader(options.isDev);
+
+  const babelLoader = buildBabelLoader(options);
 
   const fileLoader = {
     test: /\.(png|jpe?g|gif)$/i,
@@ -21,28 +26,6 @@ export function buildRules(options: BuildOptions): webpack.RuleSetRule[] {
     test: /\.tsx?$/,
     use: 'ts-loader',
     exclude: /node_modules/,
-  };
-
-  const cssLoader = buildCssLoader(options.isDev);
-
-  const babelLoader = {
-    test: /\.(js|jsx|tsx)$/,
-    exclude: /node_modules/,
-    use: {
-      loader: 'babel-loader',
-      options: {
-        presets: ['@babel/preset-env'],
-        plugins: [
-          [
-            'i18next-extract',
-            {
-              locales: ['ru', 'en'],
-              keyAsDefaultValue: true,
-            },
-          ],
-        ],
-      },
-    },
   };
 
   return [fileLoader, svgLoader, babelLoader, typescriptLoader, cssLoader];
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,11 @@
-import { ErrorBoundary } from 'app/providers/ErrorBoundary';
-import StoreProvider from 'app/providers/StoreProvider/ui/StoreProvider';
-import { ThemeProvider } from 'app/providers/ThemeProvider';
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from 'app/providers/ThemeProvider';
+import { StoreProvider } from 'app/providers/StoreProvider';
 import App from './app/App';
-
-import 'shared/config/i18n/i18n';
-import './app/styles/index.scss';
+import 'app/styles/index.scss';
+import './shared/config/i18n/i18n';
+import { ErrorBoundary } from './app/providers/ErrorBoundary';
 
 render(
   <BrowserRouter>
