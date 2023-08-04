6d1de07641bc61c9b65de72d795f0ff086c24567
Verhusha Oleg <olegverhusha@gmail.com>
2023-08-04 20:47:06 +0300

    add storybook theme addon

--- config/storybook/main.ts
+++ config/storybook/main.ts
@@ -1,7 +1,17 @@
 import type { StorybookConfig } from '@storybook/react-webpack5';
 const config: StorybookConfig = {
   stories: ['../../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
+  addons: [
+    '@storybook/addon-links',
+    {
+      name: '@storybook/addon-essentials',
+      options: {
+        backgrounds: false,
+      },
+    },
+    '@storybook/addon-interactions',
+    'storybook-addon-themes',
+  ],
   framework: {
     name: '@storybook/react-webpack5',
     options: {},
--- config/storybook/preview.ts
+++ config/storybook/preview.ts
@@ -1,11 +1,10 @@
 import type { Preview } from '@storybook/react';
 
-import { Theme } from './../../src/app/providers/ThemeProvider';
 import { RouterDecorator } from './../../src/shared/config/storybook/RouterDecorator/RouterDecorator';
 import { StyleDecorator } from './../../src/shared/config/storybook/StyleDecorator/StyleDecorator';
 import { SuspenseDecorator } from './../../src/shared/config/storybook/SuspenseDecorator/SuspenseDecorator';
 import { ThemeDecorator } from './../../src/shared/config/storybook/ThemeDecorator/ThemeDecorator';
-
+import { Theme } from './../../src/shared/const/theme';
 
 const preview: Preview = {
   parameters: {
@@ -15,6 +14,15 @@ const preview: Preview = {
         color: /(background|color)$/i,
         date: /Date$/,
       },
+    },
+    layout: 'fullscreen',
+    themes: {
+      default: 'light',
+      list: [
+        { name: 'light', class: Theme.LIGHT, color: '#ffffff' },
+        { name: 'dark', class: Theme.DARK, color: '#000000' },
+        { name: 'orange', class: Theme.ORANGE, color: '#ffb005' },
+      ],
     },
   },
   decorators: [StyleDecorator, RouterDecorator, ThemeDecorator(Theme.LIGHT), SuspenseDecorator],
